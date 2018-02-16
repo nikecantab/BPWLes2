32f74fe1e51ea9a3cd805d2689731bf13860db4d
nikecantab <nikecanalicchio@gmail.com>
2018-02-16 19:40:29 +0100

    color change script added

---
+++ BPW2/Assets/Scripts/ColorChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[ExecuteInEditMode]
+public class ColorChange : MonoBehaviour {
+
+    public Color cubeColor = new Color();
+
+    public Renderer cubeRenderer;
+
+	// Use this for initialization
+	void Start () {
+        cubeRenderer.material.color = cubeColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (cubeRenderer.material.color != cubeColor)
+        {
+            cubeRenderer.material.color = cubeColor;
+        }
+    }
+}
