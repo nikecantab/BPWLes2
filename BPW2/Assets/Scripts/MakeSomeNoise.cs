f5fd0a9ebdabb5aa97782617a50fb4ecaaae3b86
nikecantab <nikecanalicchio@gmail.com>
2018-02-19 22:13:51 +0100

    more imports + lowpoly waves

---
+++ BPW2/Assets/Scripts/MakeSomeNoise.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeSomeNoise : MonoBehaviour {
+    public float power = 3;
+    public float scale = 1;
+    public float timeScale = 1;
+
+    private float offsetX;
+    private float offsetY;
+    private MeshFilter mf;
+
+	// Use this for initialization
+	void Start () {
+		mf = GetComponent<MeshFilter>();
+        MakeNoise();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MakeNoise();
+        offsetX += Time.deltaTime * timeScale;
+        offsetY += Time.deltaTime * timeScale;
+    }
+
+    void MakeNoise()
+    {
+        Vector3[] vertices = mf.mesh.vertices;
+
+        for (int i=0; i<vertices.Length; i++)
+        {
+            vertices[i].y = CalculateHeight(vertices[i].x, vertices[i].z)*power;
+        }
+
+        mf.mesh.vertices = vertices;
+    }
+
+    float CalculateHeight(float x, float y)
+    {
+        float xCord = x * scale + offsetX;
+        float yCord = y * scale + offsetY;
+
+        return Mathf.PerlinNoise(xCord,yCord);
+    }
+}
