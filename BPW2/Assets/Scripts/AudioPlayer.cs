4c1e33a87c8d38039c346bbe6f3afd405cdbb80c
nikecantab <nikecanalicchio@gmail.com>
2018-02-19 18:15:56 +0100

    added low poly + probuild packs
    and test levels

---
+++ BPW2/Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour {
+
+    private AudioSource windSource;
+
+	// Use this for initialization
+	void Start () {
+        //casting a var: cast to another type(AudioSource)
+        windSource = (AudioSource)GetComponent(typeof(AudioSource));
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        windSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+            windSource.Play();
+    }
+}
